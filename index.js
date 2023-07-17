a67ffc53227f6e41c10882d412f57d2ad5bfa4d6
Averette <averettewjohnson@gmail.com>
2023-07-17 10:24:18 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+];
+const superbowlWin = (collection) =>{
+    for(const ele of collection){
+        if(ele.result === "W"){
+           return ele.year 
+        }
+    }
+};
